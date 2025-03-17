0f7e0eb8713dd0cf591ddc172dc9091dfb8c6ef5
adiljamil28 <110780060+adiljamil28@users.noreply.github.com>
2025-03-17 04:34:21 +0000

    Prefix and Postfix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,4 +24,61 @@
 let newData = "ascascas"
 let secData = Boolean (newData)
 
-console.log(typeof secData);
+// console.log(typeof secData);
+
+
+// ************ Operations *******************
+
+let value = 52;
+let negValue =-value
+//  console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " adil"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log(1 + "2" + 3);
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gameCounter1 = 100;
+gameCounter1++
+
+let gameCounter2 = 100;
+++gameCounter2
+
+// console.log(gameCounter1);
+// console.log(gameCounter2);
+
+
+// *****prefix******
+
+// prefix may "a" ki jo value main ++ hone k baad us ki value "b" ko assign ho rahi hai jis ki surat may "b" ko increment hui v value milti hai tw is waja se "a" or "b" ki values same he rehti hai or dono may koi fark nai hota. 
+
+
+let a = 3
+let b = ++a
+
+console.log("prefix");
+console.log(a);
+console.log(b);
+
+// ***** Postfix ******
+// Postfix may valOne ki jo value hai wo valTwo ko pehle assign ho rahi ha is liye valTwo may koi change nai araha us k baad valOne may ++ yani increment ho raha hai jis ki surat may valone ++ hojata ha magar valTwo ki value pehle assign hone ki waja se same he rehti ha us may koi fark nai ayega.
+
+console.log("Postfix");
+let valOne = 8
+let valTwo =valOne++
+
+console.log(valOne);
+console.log(valTwo);
