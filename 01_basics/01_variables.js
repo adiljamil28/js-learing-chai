e675cb4f930585b318463f1cfad6fa7a4dedad95
adiljamil28 <110780060+adiljamil28@users.noreply.github.com>
2025-03-17 02:05:54 +0000

    learing variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId =144552;
+let userEmail ="adil@gmail.com"
+var accountPass ="12345"
+accountCity = "karachi"
+let accountState;
+
+// accountId = 15 // const value changes not allowed
+
+userEmail = "ad@yahoo.com"// let variables value can be changes easily
+accountPass = 525252 // var value also can be change
+accountCity = "lahore" //without type varible values can also change easily.
+// console.log(accountId);
+// console.log(userEmail);
+// console.log(accountPass);
+// console.log(accountCity);
+
+console.table([accountId,userEmail,accountPass,accountCity,accountState]);
