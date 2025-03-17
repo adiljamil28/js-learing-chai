0528e54540782b69a1beafe42b38e2fe6f65ee40
adiljamil28 <110780060+adiljamil28@users.noreply.github.com>
2025-03-17 06:49:21 +0000

    Stack and Heap Memory

--- 01_basics/05_datatypes-summary.js
+++ 01_basics/05_datatypes-summary.js
@@ -12,7 +12,7 @@ let stockSharesAmount = 531683846864646846846846846464n //BigInt
 let userId = Symbol('123') //Symbol
 let anotherId = Symbol('123') //Symbol
 
-console.log(userId==anotherId);
+// console.log(userId==anotherId);
 
 
 // Reference or non-primitive data types
@@ -37,12 +37,12 @@ const detailUser =
 // *************** Function ***************
 
 let myFirstFunction = function(){
-    console.log("Hello world");
+    // console.log("Hello world");
 }
 myFirstFunction();
 
 
-console.log(typeof myFirstFunction);
+// console.log(typeof myFirstFunction);
 
 
 
@@ -61,6 +61,49 @@ console.log(typeof myFirstFunction);
 //        Arrays  =>  object
 //        Function  =>  function
 //        Object  =>  object
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// type of memory in javaScript
+
+
+// 1- Stack Memory use in (Primitive)
+// 2-Heap Memory use in (Non-Primitive)
+
+
+// Stack Memory
+
+let learnCourse = "javaScript"
+
+let newCourse = learnCourse
+newCourse = "java"
+
+// console.log(learnCourse);
+// console.log(newCourse);
+
+
+// ============== Heap Memory ================
+
+let myData = {
+    name :  "adil",
+    age : 30,
+    class :"beginner"
+}
+let anotherData = myData
+anotherData.age=21
+
+    console.log(anotherData.age);
+    console.log(myData.age);
+
+
+
+
+// const changeData = myData.age = 20
+// console.log(myData.age);
+// console.log(changeData);
+
+// console.log(myData.age,myData.class,myData.name);
+
 
 
 
