9efa5c5632fb7c4de300ce721d394881e960158b
adiljamil28 <110780060+adiljamil28@users.noreply.github.com>
2025-03-17 02:29:36 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all code like newer version
+
+// type of variables 
+
+// 1- String
+// 2- Number
+// 3- Boolean
+// 4- BigInt
+// 5- null
+// 6- undefined
+// 7- symbol
+
+
+let Name = "adil"
+let age = 30
+let adat = true
+let acc_Bal = 101010101010101.252521225
+
+
+// console.table(typeof adat);
+
+console.table(typeof null); //object
+
+console.table(typeof undefined); // undefined
