39a021b7730159247ae52864f41a3105f3b30d98
adiljamil28 <110780060+adiljamil28@users.noreply.github.com>
2025-03-19 02:35:42 +0000

    math method

--- 01_basics/07_number_and_math.js
+++ 01_basics/07_number_and_math.js
@@ -15,7 +15,33 @@
 
 
 let score = 15;
-console.log(score.pow(3));
+// console.log(score.pow());
+// console.log(Math.pow(score, 2)); 
+answer : 225
+
+// console.log(Math.pow(score, 3)); 
+answer : 3375
+
+let newScore = 16;
+// console.log(Math.sqrt(newScore)); 
+answer : 4
+
+let newScore1 = -5;
+console.log(Math.abs(newScore1)); 
+answer : 5
+
+let newScore2;
+// console.log(Math.random(newScore2)); 
+answer : "give you a number between 0 to 1"
 
 
+let newScore3 = 1.365165161;
+// console.log(Math.round(newScore3)); 
+answer : 1
+
+
+let num = 12.34567;
+console.log(num.toFixed(2)); // "12.35"  
+console.log(num.toFixed(4)); // "12.3457"  
+console.log(num.toFixed(0)); // "12" 
 
